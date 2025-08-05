2eb40555f30613f1a9ebcb5ecb0b0593731772b3
hamed2396 <hamed2396@gmail.com>
2025-08-05 15:21:54 +0330

    add comment logic is being implemented

--- app/src/main/java/com/example/bazar/ui/features/product/ProductScreen.kt
+++ app/src/main/java/com/example/bazar/ui/features/product/ProductScreen.kt
@@ -2,6 +2,7 @@ package com.example.bazar.ui.features.product
 
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -40,6 +41,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -114,14 +119,95 @@ fun ProductItem(
 
             )
         ProductDetail(data, comments.size.toString())
+        ProductComments(comments) {
+
+        }
+    }
+}
+
+@Composable
+fun ProductComments(comments: List<Comment>, onAddCommentClicked: () -> Unit) {
+
+    var showDialog by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    if (comments.isNotEmpty()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Comments",
+                fontWeight = FontWeight.Medium,
+                fontSize = 20.sp
+            )
+            TextButton(onClick = {
+                if (NetworkChecker(context).isInternetConnected) {
+                    showDialog = true
+                } else {
+                    Toast.makeText(context, "connect to internet", Toast.LENGTH_SHORT).show()
+                }
+            }) {
+                Text(
+                    text = "add new Comment",
+                    color = blue,
+                    fontSize = 14.sp
+                )
+            }
+
+        }
+        comments.forEach {
+            CommentsBody(it)
+        }
+    }else{
+        TextButton(onClick = {
+            if (NetworkChecker(context).isInternetConnected) {
+                showDialog = true
+            } else {
+                Toast.makeText(context, "connect to internet", Toast.LENGTH_SHORT).show()
+            }
+        }) {
+            Text(
+                text = "add new Comment",
+                color = blue,
+                fontSize = 14.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun CommentsBody(comment: Comment) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 8.dp),
+        border = BorderStroke(1.dp, Color.LightGray),
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 0.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+        shape = shapes.large
+    ) {
+
+        Column(modifier = Modifier.padding(12.dp)) {
+            Text(
+                text = comment.userEmail,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                modifier = Modifier.padding(top = 10.dp),
+                text = comment.text,
+                fontSize = 14.sp,
+
+                )
+        }
     }
 }
 
 @Composable
 fun ProductDetail(product: Product, commentNumber: String) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween
+        modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Column {
             Row(verticalAlignment = Alignment.CenterVertically) {
@@ -166,8 +252,7 @@ fun ProductDetail(product: Product, commentNumber: String) {
                     fontSize = 13.sp
                 )
                 Row(
-                    horizontalArrangement = Arrangement.End,
-                    modifier = Modifier.fillMaxWidth()
+                    horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()
                 ) {
                     Surface(color = blue, shape = shapes.large) {
                         Text(
