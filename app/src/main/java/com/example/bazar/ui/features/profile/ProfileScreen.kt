63725498155bd1400a5b55224e233e1ba04ab4c5
hamed2396 <hamed2396@gmail.com>
2025-08-06 14:21:09 +0330

    ANIMATION for profile is done

--- app/src/main/java/com/example/bazar/ui/features/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/bazar/ui/features/profile/ProfileScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -25,6 +26,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Composition
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,6 +40,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.bazar.R
 import com.example.bazar.model.data.Product
 import com.example.bazar.ui.theme.blue
 import com.example.bazar.ui.theme.shapes
@@ -77,7 +85,14 @@ fun UserDataSection() {
 
 @Composable
 fun MainAnimation() {
-
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.profile_anim))
+    LottieAnimation(
+        modifier = Modifier
+            .size(240.dp)
+            .padding(top = 36.dp, bottom = 16.dp),
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
